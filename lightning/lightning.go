ad82e1ad296e615d053546c3f712c2901c2301ae
gohumble <lngohumble@gmail.com>
2022-10-13 20:43:50 +0200

    comments

--- lightning/lightning.go
+++ lightning/lightning.go
@@ -4,27 +4,35 @@ import (
 	"fmt"
 )
 
+// If these interfaces are not implemented, library users of cashu MUST provide the lightning capabilities them self.
+// Please be sure of not enabling lightning in the configuration, if you provide lightning services yourself.
+// When using cashu as stand alone, this lightning interface could be provided by multiple lighting services like
+// LND, CLN, or any abstraction layer like LNBits.
+
+// Invoice should create a lightning invoice somewhere.
 type Invoice interface {
-	fmt.Stringer
-	SetHash(h string)
-	GetHash() string
+	fmt.Stringer      // toJson
+	SetHash(h string) // set the payment hash
+	GetHash() string  // get the payment hash
 
-	SetIssued(i bool)
-	IsIssued() bool
+	SetIssued(i bool) // SetIssued to true, if lightning invoice was paid
+	IsIssued() bool   // IsIssued returns true, if lightning invoice is paid
 
-	SetAmount(a int64)
-	GetAmount() int64
+	SetAmount(a int64) // SetAmount of the lightning invoice
+	GetAmount() int64  // GetAmount of the lightning invoice
 
-	GetPaymentRequest() string
+	GetPaymentRequest() string // GetPaymentRequest should return the payment request (probably bech encoded)
 }
 
+// Payment should give information about the payment status
 type Payment interface {
-	IsPaid() bool
-	GetPreimage() string
+	IsPaid() bool        // IsPaid must return true, if payment is fulfilled
+	GetPreimage() string // GetPreimage must return the preimage of the payment
 }
 
+// Client should be able to perform lightning services
 type Client interface {
-	InvoiceStatus(paymentHash string) (Payment, error)
-	Pay(paymentRequest string) (Invoice, error)
-	CreateInvoice(amount int64, memo string) (Invoice, error)
+	InvoiceStatus(paymentHash string) (Payment, error)        // InvoiceStatus should return Payment information for a payment hash
+	Pay(paymentRequest string) (Invoice, error)               // Pay should pay the payment request.
+	CreateInvoice(amount int64, memo string) (Invoice, error) // CreateInvoice should create an invoice for given amount and memo
 }
